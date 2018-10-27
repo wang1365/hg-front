f008f040b8c252dc8fbbdbde4d8e11220972d45d
wontfalling <rongjun.gao@foxmail.com>
2018-10-27 20:23:33 +0800

    area: load areas from backend

--- src/views/area/index.vue
+++ src/views/area/index.vue
@@ -5,13 +5,13 @@
       <el-button type="success" icon="el-icon-plus" size="small" class="right-btn blue-btn" @click="createArea">新增</el-button>
     </el-row>
     <el-row class="table">
-      <el-table :data="items" size="small" border stripe highlight-current-row>
+      <el-table :data="items" border stripe highlight-current-row>
         <el-table-column prop="id" label="ID" width="100" />
         <el-table-column prop="name" label="片区名称" width="220" />
         <el-table-column prop="enabled" label="启用与否" width="220" />
         <el-table-column prop="chargeOrg" label="所属机构" />
         <el-table-column prop="chargeOwner" label="负责人姓名" width="220" />
-        <el-table-column prop="phone" label="负责人手机号码" width="220" />
+        <el-table-column prop="ownerPhone" label="负责人手机号码" width="220" />
         <el-table-column label="操作" width="200" >
           <template slot-scope="scope">
             <el-button size="mini" type="warning" @click="showArea(scope.row.id)">详情</el-button>
@@ -24,12 +24,18 @@
 </template>
 
 <script>
+import { getAreaList } from '@/api/area'
 import AreaDialog from './AreaDialog'
 
 export default {
   name: 'Area',
   components: {
     AreaDialog
+  },
+  created() {
+    getAreaList().then(response => {
+      this.items = response.data.data
+    })
   },
   methods: {
     showAreaDialog(data, action) {
